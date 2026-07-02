16fca6537dcc02bbcbb02c39af37ff117b1ac134
Sakshi Chourasia <148641205+Sakshichourasia13@users.noreply.github.com>
2026-07-02 19:32:53 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        a=[]
+        for i in nums:
+            if i in a:
+                return i
+            a.append(i)
